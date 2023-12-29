577ca0791218299a4a5ff164a0f1cab98aa207f6
Youssef <youssefmelad1986@gmail.com>
2023-12-29 09:53:55 +0300

    Changed font to FiraCode Nerd Mono

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ require("humongousDude.lazy")
 
 if vim.g.neovide then
 
-    vim.o.guifont = "JetBrainsMono_Nerd_Font_Mono:h12"
+    --vim.o.guifont = "JetBrainsMono_Nerd_Font_Mono:h12"
+    vim.o.guifont = "FiraCode_Nerd_Font_Mono:h12"
 
 end
